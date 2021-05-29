f6a4f53489c2fc91b29d171f4a1c592feb51763b
Taehee Kim <wonderfulhuman@naver.com>
2021-05-30 03:14:02 +0900

    modal

--- src/App.js
+++ src/App.js
@@ -1,25 +1,72 @@
+import {useState} from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+
+
+  let cnt = 1;
+  const [a,b] = useState(1);
+  const [title,fun] = useState(['남자 코드추천', '여자코트추천', '강남역삼']);
+  const [modal, modalf] = useState(false);
+
+  function ff() {
+    const newarr = [...title];
+    newarr.sort();
+    fun(newarr);
+  }
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="navs">
+        네브바
+      </div>
+
+      <button type='button' onClick= {
+
+          ff
+      }>버튼 </button>
+
+      <div className="title"
+      onClick = {
+
+        () => {
+          modalf(!modal);
+        }
+
+      }>
+        클릭 ㄱ
+      </div>
+
+      <div className="list">
+        <h3 className="title">{title} <span onClick={
+          () => {
+            b(a+1);
+          }
+        }>😂</span> {a} </h3>
+        <p>김태희입니다.</p>
+      </div>
+
+         {
+           modal === true 
+           ? <Modal></Modal> 
+           : null
+         }
+
     </div>
   );
+}
+
+function Modal() {
+  return (
+
+    <div className="modal">
+      <h2>제목</h2>
+      <p>날짜</p>
+      <p>상세내용</p>
+    </div>
+  )
 }
 
 export default App;
